e0b706857bbc22756e32832c29ce1b676922e12c
gurdeep098 <g.gursimransingh@myauctioneer.eu>
2025-10-08 15:05:24 +0200

    "created the end point goats.js "

--- index.js
+++ index.js
@@ -1,49 +1,10 @@
 const express = require("express");
-
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello World');
-})
+// all the routes
 
-
-const goats = [
-    {
-      "id":1,
-      "name":"t-rex"
-    },
-    {
-      "id":2,
-      "name":"connie"
-    },
-    {
-      "id":3,
-      "name":"clyde"
-    },
-  ]
-
-app.get('/goats', (req, res) => { 
-  res.json(goats);
-})
-
-app.get('/goats/:id', (req, res) => { 
-  console.log(req.params.id);
-  let selectedGoat = null;
-
-  goats.forEach((goat) => { 
-    if (goat.id == req.params.id) { 
-      selectedGoat = goat;
-    }
-  });
-
-  res.json(selectedGoat);
-});
-
-app.delete('/goats/:id', (req, res) => { 
-  console.log("Delete the goat with id" + req.params.id);
-  res.send("Delete the goat");
-})
-
+const goatsRouter = require('./routes/goats');
+app.use('/goats',goatsRouter);
 
 console.log("...SERVER IS RUNNING...");
 
---
+++ routes/goats.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const router = express.Router();
+
+
+
+const goats = [
+    {
+      "id":1,
+      "name":"t-rex"
+    },
+    {
+      "id":2,
+      "name":"connie"
+    },
+    {
+      "id":3,
+      "name":"clyde"
+    },
+  ]
+
+router.get('/', (req, res) => { 
+  res.json(goats);
+})
+
+router.get('/:id', (req, res) => { 
+  console.log(req.params.id);
+  let selectedGoat = null;
+
+  goats.forEach((goat) => { 
+    if (goat.id == req.params.id) { 
+      selectedGoat = goat;
+    }
+  });
+
+  res.json(selectedGoat);
+});
+
+router.delete('/:id', (req, res) => { 
+  console.log("Delete the goat with id" + req.params.id);
+  res.send("Delete the goat");
+})
+
+module.exports = router;
