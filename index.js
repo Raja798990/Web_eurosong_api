b294e0c5d63e989266e85a4c3e942d0c8603de2a
gurdeep098 <g.gursimransingh@myauctioneer.eu>
2025-10-08 14:16:23 +0200

    "postman - get code added"

--- index.js
+++ index.js
@@ -6,6 +6,25 @@ app.get('/', (req, res) => {
   res.send('Hello World');
 })
 
+app.get('/goats', (req, res) => {
+  const goats = [
+    {
+      "id":1,
+      "name":"t-rex"
+    },
+    {
+      "id":2,
+      "name":"connie"
+    },
+    {
+      "id":3,
+      "name":"clyde"
+    },
+  ]
+  res.json(goats);
+})
+
 console.log("...SERVER IS RUNNING...");
 
 app.listen(3000);
+
